5b7bffd0bb446b3a8494e288416a0e241dcf704f
Tonu Cristian <cristian.tonu@iongroup.com>
2025-11-10 17:40:18 +0200

    working on pagination

--- src/app/common/pagination/pagination.ts
+++ src/app/common/pagination/pagination.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
 
 import { Button } from '../button/button';
 
@@ -14,6 +14,8 @@ export class Pagination {
   isProductsLoading = input.required<boolean>();
   prev = output();
   next = output();
+
+  pages = computed(() => [...Array(this.totalPages()).keys()].map((_, i) => i));
 
   prevPage() {
     this.prev.emit();
